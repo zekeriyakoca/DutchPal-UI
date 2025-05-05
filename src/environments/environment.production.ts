2b2ce524544d85b8f50431fbba17be1dc394cc46
zekeriya kocairi <zkocairi@skillstown.com>
2025-05-05 14:33:16 +0200

    minor change

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'https://nest.lontray.shop/dutchpal-api',
-
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8000',
-  apiUrl: 'https://nest.lontray.shop/dutchpal-api',
+  apiUrl: 'http://localhost:8000',
+  // apiUrl: 'https://nest.lontray.shop/dutchpal-api',
 
 };
